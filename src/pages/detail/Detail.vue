fcec37ebb1e5167c77b536b2d86ad6d8c395fcd3
Primers_cern <Primers_cern@outlook.com>
2018-07-17 17:46:12 +0800

    detail-ajax

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <detail-banner></detail-banner>
+    <detail-banner
+      :sightName="sightName"
+      :bannerImg="bannerImg"
+      :gallaryImgs="gallaryImgs"
+    ></detail-banner>
     <detail-header></detail-header>
-    <detail-list :list="list"></detail-list>
+    <detail-list :list="categoryList"></detail-list>
   </div>
 </template>
 
@@ -10,6 +14,7 @@
   import DetailBanner from './components/Banner'
   import DetailHeader from './components/Header'
   import DetailList from './components/List'
+  import axios from 'axios'
 
   export default {
     name: "Detail",
@@ -20,26 +25,31 @@
     },
     data () {
       return {
-        list: [
-        {
-          title: '成人票',
-          children: [
-            {
-              title: '成人三联票',
-              children: [{
-                title: '第三个子元素'
-              }]
-            },
-            {title: '成人五联票'}
-          ]
-        }, {
-          title: '学生票'
-        }, {
-          title: '儿童票'
-        }, {
-          title: '特惠票'
-        }]
+        sightName: '',
+        bannerImg: '',
+        gallaryImgs: [],
+        categoryList: []
       }
+    },
+    methods: {
+      getDetailInfo () {
+        axios.get('api/detail.json', {
+          params: { id: this.$route.params.id }
+        }).then(this.handleGetDataSucc)
+      },
+      handleGetDataSucc (res) {
+        res = res.data
+        if (res.ret && res.data) {
+          const data = res.data
+          this.sightName = data.sightName
+          this.bannerImg = data.bannerImg
+          this.gallaryImgs = data.gallaryImgs
+          this.categoryList = data.categoryList
+        }
+      }
+    },
+    mounted () {
+      this.getDetailInfo()
     }
   }
 </script>
